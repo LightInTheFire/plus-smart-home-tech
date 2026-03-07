a577d5270dfd72aeaefc3a33d1a9bdcd2ddf1747
Ilia Egorov <aleilmessia@mail.ru>
2026-03-08 00:14:42 +0300

    feat: update scripts to correctly show text in terminal

--- hub-router/scripts/windows/4-analyzer-tests.bat
+++ hub-router/scripts/windows/4-analyzer-tests.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
 
+chcp 65001 >nul
+
 REM Локальный запуск тестов Hub Router для проверки полного комплекса Collector (grpc) + Aggregator + Analyzer
 
 set "JAR_PATH=%~dp0..\hub-router.jar"
