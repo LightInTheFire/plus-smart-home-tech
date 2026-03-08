572bceb992a5e59b070bb4d9c83ad7d8ff249da6
Ilia Egorov <aleilmessia@mail.ru>
2026-03-08 12:29:28 +0300

    feat: remove devtools

--- telemetry/analyzer/src/test/java/ru/yandex/practicum/testcontainers/TestcontainersAnalyzerApp.java
+++ telemetry/analyzer/src/test/java/ru/yandex/practicum/testcontainers/TestcontainersAnalyzerApp.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.testcontainers;
 import ru.yandex.practicum.AnalyzerApp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +13,6 @@ public class TestcontainersAnalyzerApp {
 
     @Bean
     @ServiceConnection
-    @RestartScope()
     PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>("postgres:18.3-alpine").withUsername("postgres")
             .withPassword("secret")
