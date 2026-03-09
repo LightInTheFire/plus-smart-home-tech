f903ba3c756cfa0d7afd508fdcbb5dd01992e932
Ilia Egorov <aleilmessia@mail.ru>
2026-03-09 12:23:26 +0300

    spotless apply

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/service/SnapshotCacheServiceImpl.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/service/SnapshotCacheServiceImpl.java
@@ -53,7 +53,6 @@ public class SnapshotCacheServiceImpl implements SnapshotCacheService {
             .put(event.getId(), newState);
         sensorsSnapshotAvro.setTimestamp(event.getTimestamp());
 
-
         return Optional.of(sensorsSnapshotAvro);
     }
 }
