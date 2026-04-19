624ad502294787fe12928b296d1264b20a0f6503
Ilia Egorov <aleilmessia@mail.ru>
2026-04-19 07:02:04 +0300

    feat: add new services db init

--- init-db/init-multiple-databases.sh
+++ init-db/init-multiple-databases.sh
@@ -5,4 +5,7 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE "shopping-cart";
     CREATE DATABASE "shopping-store";
     CREATE DATABASE "warehouse";
+    CREATE DATABASE "order";
+    CREATE DATABASE "payment";
+    CREATE DATABASE "delivery";
 EOSQL
